b2b36723f802e4aa4edee206a71d6b7b2c2bce59
mohdshahzil <theshahzil@gmail.com>
2024-03-06 17:39:43 +0530

    data model and dummy data

---
+++ lib/data/questions.dart
@@ -0,0 +1,19 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestion('How are Flutter UIs build', [
+    'By combining widgets in code',
+    'By combining widgets in visual editor',
+    'By defining widgets in config files',
+    'By using Xcode for ios and Android Studio for Android',
+  ])
+];
