6956f2ce4a992caa5c8d27384447c2e4111f630e
mohdshahzil <theshahzil@gmail.com>
2024-03-08 02:52:10 +0530

    expanded added

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -12,13 +12,15 @@ class QuestionsSummary extends StatelessWidget {
         return Row(
           children: [
             Text(((data['questions_index'] as int) + 1).toString()),
-            Column(
-              children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5),
-                Text(data['user_answer'] as String),
-                Text(data['correct_answer'] as String),
-              ],
+            Expanded(
+              child: Column(
+                children: [
+                  Text(data['question'] as String),
+                  const SizedBox(height: 5),
+                  Text(data['user_answer'] as String),
+                  Text(data['correct_answer'] as String),
+                ],
+              ),
             ),
           ],
         );
